9a40ab640c11d21007f4e0532a46848fc2983d1b
ABeltramo <beltramo.ale@gmail.com>
2016-05-13 10:28:44 +0200

    Initial Commit

---
+++ CompressioneDCT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CompressioneDCT
+//
+//  Created by Ale on 13/05/16.
+//  Copyright © 2016 ABeltramo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}

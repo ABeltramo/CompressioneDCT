df31400208046cc824f4df193718930c9dd50a26
ABeltramo <beltramo.ale@gmail.com>
2016-05-22 18:25:24 +0200

    Added necessary library

--- autoconfig.sh
+++ autoconfig.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get update
-apt-get install -y g++ gtk+3 libgtk-3-dev libfftw3-3 libfftw3-dev
+apt-get install -y g++ gtk+3.0 libgtk-3-dev libfftw3-3 libfftw3-dev
 echo "Ready to make"
